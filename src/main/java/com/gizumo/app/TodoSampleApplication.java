47cc8a96120179ad596a44f6be3550bd7d8fc451
Daichi Endo <dddpga1@gmail.com>
2017-11-04 12:15:00 +0900

    first commit

---
+++ src/main/java/com/gizumo/app/TodoSampleApplication.java
@@ -0,0 +1,12 @@
+package com.gizumo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSampleApplication.class, args);
+	}
+}
